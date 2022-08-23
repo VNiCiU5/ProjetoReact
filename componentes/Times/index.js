c7f4948461296221ba8123e242ce1b4d34210bdc
Vinicius <viniciuspontesdoliveira@gmail.com>
2022-08-23 14:19:13 -0300

    adicionando estilos ao componente time

--- componentes/Times/index.js
+++ componentes/Times/index.js
@@ -3,8 +3,8 @@ import './Times.css'
 const Time = (props) =>{
 
     return (
-        <section className='times'>
-            <h3>{props.nome}</h3>
+        <section className='times' style={{backgroundColor: props.corSecundaria}}>
+            <h3 style={{borderColor: props.corPrimaria}}> {props.nome} </h3>
         </section>
     )
 
